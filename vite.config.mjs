50d1f53d5229ef607bf0c80d1de0bfa0bfb991bb
gedev <geniilsonfernandes@gmail.com>
2024-12-23 09:42:51 -0300

    chore: update package.json and vite.config.mjs for improved testing and coverage

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
@@ -8,5 +8,22 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: './vitest.setup.mjs',
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'html'],
+      include: ['src/**/*.{ts,tsx}'],
+      exclude: [
+        // 'src/**/*.d.ts',
+        'src/**/*.test.{ts,tsx}',
+        // 'src/**/*.spec.{ts,tsx}',
+        'src/**/*.stories.{ts,tsx}',
+        // 'src/main.tsx',
+      ],
+      all: true,
+      statements: 80,
+      branches: 80,
+      functions: 80,
+      lines: 80,
+    },
   },
 });
