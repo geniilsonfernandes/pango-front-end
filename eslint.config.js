7bfd7548ed1001b8fb4e66671d01468928973318
gedev <geniilsonfernandes@gmail.com>
2024-12-30 17:24:41 -0300

    style: format configuration files for consistency; remove unused ESLint rules

--- eslint.config.js
+++ eslint.config.js
@@ -4,12 +4,4 @@ import tseslint from 'typescript-eslint';
 
 export default tseslint.config(...mantine, {
   ignores: ['**/*.{mjs,cjs,js,d.ts,d.mts}', './.storybook/main.ts'],
-  plugins: ['@tanstack/query'],
-  extends: ['plugin:@tanstack/query/recommended'],
-  rules: {
-    '@tanstack/query/exhaustive-deps': 'error',
-    '@tanstack/query/no-deprecated-options': 'error',
-    '@tanstack/query/prefer-query-object-syntax': 'error',
-    '@tanstack/query/stable-query-client': 'error',
-  },
 });
