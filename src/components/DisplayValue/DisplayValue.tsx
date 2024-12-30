237ef3a66ac263ebf686696fa9b1ec487d20ad21
gedev <geniilsonfernandes@gmail.com>
2024-12-30 17:24:34 -0300

    feat: add DisplayValue component for displaying labeled values; implement ListActions component for list management actions

---
+++ src/components/DisplayValue/DisplayValue.tsx
@@ -0,0 +1,19 @@
+import { Box, rem, Text } from '@mantine/core';
+
+type DisplayValueProps = {
+  value: string | number;
+  label: string;
+} & React.HTMLAttributes<HTMLDivElement>;
+
+export const DisplayValue: React.FC<DisplayValueProps> = ({ value, label, ...props }) => {
+  return (
+    <Box {...props}>
+      <Text fz={rem(10)} tt="uppercase" fw={700} c="dimmed">
+        {label}
+      </Text>
+      <Text fz={rem(14)} fw={700}>
+        {value}
+      </Text>
+    </Box>
+  );
+};
