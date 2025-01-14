072a8a8ad969e823bd8c4323d07285bec8a8eff0
gedev <geniilsonfernandes@gmail.com>
2025-01-14 17:09:44 -0300

    feat: add PrintableList component for generating printable shopping lists

---
+++ src/components/PrintableList/PrintableList.tsx
@@ -0,0 +1,160 @@
+import { useMemo } from 'react';
+import { IconCheckbox } from '@tabler/icons-react';
+import { useQueryClient } from '@tanstack/react-query';
+import { Text, Title } from '@mantine/core';
+import { ListDTO, ShoppingItem } from '@/service/api';
+import { listQueryKeys } from '@/service/queries/useLists';
+import { calculateStatus } from '@/utils/calculateStatus';
+import { ListStats } from '../ListStats/ListStats';
+import { Logo } from '../Logo/Logo';
+import classes from './PrintableList.module.css';
+
+type PrintableListProps = {
+  list: ListDTO;
+  grouped: boolean;
+  uncheckedOnly: boolean;
+  showStatus: boolean;
+};
+
+const Item: React.FC<{ item: ShoppingItem }> = ({ item }) => {
+  const shortenName = (name: string) => (name.length > 30 ? `${name.slice(0, 30)}...` : name);
+
+  return (
+    <div style={{ display: 'flex', gap: '10px' }}>
+      <Text size="xs" c="#7B7B7B">
+        {item.quantity} {item.unit}
+      </Text>
+      <Text size="xs">{shortenName(item.name)}</Text>
+    </div>
+  );
+};
+
+const renderGroupedItems = (items: ListDTO['items']) => {
+  const categorized = items?.reduce(
+    (acc, item) => {
+      if (!acc[item.category]) {
+        acc[item.category] = [];
+      }
+      acc[item.category]?.push(item);
+      return acc;
+    },
+    {} as Record<string, ListDTO['items']>
+  );
+
+  return Object.entries(categorized || {}).map(([category, categoryItems]) => (
+    <div key={category}>
+      <Title order={4} my="xs">
+        {category}
+      </Title>
+      {categoryItems?.map((item) => <Item key={item.id} item={item} />)}
+    </div>
+  ));
+};
+
+const renderFlatItems = (items: ListDTO['items']) => (
+  <div
+    style={{
+      display: 'grid',
+      gridTemplateColumns: 'repeat(3, 1fr)',
+      gap: '10px',
+      padding: '16px 0',
+    }}
+  >
+    {items?.map((item) => <Item key={item.id} item={item} />)}
+  </div>
+);
+
+export const PrintableList: React.FC<PrintableListProps> = ({
+  list,
+  showStatus,
+  grouped,
+  uncheckedOnly,
+}) => {
+  const queryClient = useQueryClient();
+  const listItems = queryClient.getQueryData(listQueryKeys.listItems(list.id)) as ShoppingItem[];
+
+  const status = useMemo(() => calculateStatus(list.items), [list]);
+
+  const filterItems = (checked: boolean) =>
+    listItems?.filter((item) => item.checked === checked) || [];
+
+  return (
+    <div className={classes.printContent}>
+      {/* Header */}
+      <div className={classes.header}>
+        <Logo />
+      </div>
+
+      {showStatus && (
+        <div className={classes.stats}>
+          <ListStats
+            totalItems={`${status.checked} / ${status.total}`}
+            budget={`${list.budget}`}
+            checked={`${status.checkedPrice}`}
+            unchecked={`${status.uncheckedPrice}`}
+            progress={(status.checked / status.total) * 100}
+          />
+        </div>
+      )}
+
+      {/* Unchecked Items */}
+      <Title
+        order={6}
+        style={{
+          display: 'inline-flex',
+          gap: '10px',
+          alignItems: 'center',
+          border: '1px solid #bdbdbd',
+          padding: '16px',
+          marginTop: '32px',
+          color: '#000',
+        }}
+      >
+        <IconCheckbox /> Unchecked
+      </Title>
+      <div
+        style={{
+          display: 'grid',
+          gridTemplateColumns: 'repeat(3, 1fr)',
+          gap: '10px',
+          padding: '16px 0',
+        }}
+      >
+        {grouped ? renderGroupedItems(filterItems(false)) : renderFlatItems(filterItems(false))}
+      </div>
+
+      {/* Checked Items */}
+      {!uncheckedOnly && (
+        <>
+          <Title
+            order={6}
+            style={{
+              display: 'inline-flex',
+              gap: '10px',
+              alignItems: 'center',
+              border: '1px solid #bdbdbd',
+              padding: '16px',
+              marginTop: '32px',
+              color: '#000',
+            }}
+          >
+            <IconCheckbox /> Checked
+          </Title>
+          <div
+            style={{
+              display: 'grid',
+              gridTemplateColumns: 'repeat(3, 1fr)',
+              gap: '10px',
+              padding: '16px 0',
+            }}
+          >
+            {grouped ? renderGroupedItems(filterItems(true)) : renderFlatItems(filterItems(true))}
+          </div>
+        </>
+      )}
+
+      {/* Footer */}
+      <div className={classes.footer} />
+    </div>
+  );
+};
